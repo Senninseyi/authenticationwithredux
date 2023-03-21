b704687ebf4804ce6ff4014d33b5e87e2c972f0b
Senninseyi <seyi.oyebamiji@gmail.com>
2023-03-21 11:22:47 +0100

    Update Profilescreen.js

--- screen/Profilescreen.js
+++ screen/Profilescreen.js
@@ -2,13 +2,9 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const Profilescreen = ({ navigation }) => {
-  console.log(navigation);
 
   return (
     <View style={styles.screen}>
-      <Text style={{ color: "white", fontSize: 40, fontWeight: "bold" }}>
-        Profilescreen
-      </Text>
     </View>
   );
 };
@@ -16,7 +12,6 @@ const Profilescreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: "blue",
     alignItems: "center",
     justifyContent: "center",
     width: "100%",
