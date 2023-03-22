9180468d391dad1206e905c18ef03f036f19d09c
Senninseyi <seyi.oyebamiji@gmail.com>
2023-03-22 10:56:00 +0100

    update

--- screen/Loginscreen.js
+++ screen/Loginscreen.js
@@ -1,26 +1,147 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  Alert,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableNativeFeedback,
+  View,
+} from "react-native";
+import { globalstyles } from "../style";
+import { CustomButton } from "../component/button/custombutton";
+import { DismissKeyboard } from "../hoc/keyboardDismiss";
+import { save } from "../hoc/secureStore";
 
-const Loginscreen = ({ navigation }) => {
-  console.log(navigation);
+function SplashScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 20,
+      }}
+    >
+      <Text>Getting token...</Text>
+      <ActivityIndicator size="large" color={"red"} />
+    </View>
+  );
+}
+
+const Loginscreen = ({ navigation, route }) => {
+  const [formData, setFormdata] = useState({
+    username: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  if (loading) {
+    return <SplashScreen />;
+  }
+
+  const Signin = () => {
+    setLoading(true);
+    try {
+      setFormdata({
+        ...formData,
+        username: formData.username,
+        password: formData.password,
+      });
+      save("user", formData.username);
+      setLoading(false);
+      console.log("Data", formData);
+    } catch (error) {
+      Alert.alert("Error", error);
+    }
+  };
 
   return (
-    <View style={styles.screen}>
-      <Text style={{ color: "white", fontSize: 40, fontWeight: "bold" }}>
-        Loginscreen
-      </Text>
-    </View>
+    <DismissKeyboard>
+      <View style={styles.screen}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.textViewcontainer}
+        >
+          <TextInput
+            placeholder="username"
+            selectionColor="red"
+            style={globalstyles.textInput}
+            value={formData.username}
+            onChangeText={(username) => {
+              setFormdata({
+                ...formData,
+                username: username,
+              });
+            }}
+          />
+          <TextInput
+            placeholder="password"
+            selectionColor="red"
+            cursorColor="red"
+            secureTextEntry={true}
+            style={globalstyles.textInput}
+            value={formData.password}
+            onChangeText={(password) => {
+              setFormdata({
+                ...formData,
+                password: password,
+              });
+            }}
+          />
+
+          <CustomButton
+            onPress={() => Signin()}
+            customStyle={styles.signInbutton}
+          >
+            Sign In
+          </CustomButton>
+        </KeyboardAvoidingView>
+        <View style={styles.forgotpasswordContainer}>
+          <TouchableNativeFeedback
+            onPress={() => Alert.alert("Alert", "forgot password")}
+          >
+            <Text style={styles.forgotPassword}>forgot password?</Text>
+          </TouchableNativeFeedback>
+        </View>
+      </View>
+    </DismissKeyboard>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: "red",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
     width: "100%",
     height: 500,
+  },
+  textViewcontainer: {
+    width: "100%",
+    maxWidth: "90%",
+    padding: 5,
+    gap: 20,
+  },
+  signInbutton: {
+    width: "100%",
+    maxWidth: "25%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black",
+  },
+  forgotpasswordContainer: {
+    width: "100%",
+    maxWidth: "90%",
+    padding: 5,
+    marginTop: 20,
+  },
+  forgotPassword: {
+    color: "black",
+    fontWeight: "600",
+    fontSize: 18,
   },
 });
 
