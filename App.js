0a095d5d2ad6e68ccd0537523e5489370b738353
Senninseyi <seyi.oyebamiji@gmail.com>
2023-03-22 19:58:17 +0100

    chore: configure persistgate for redux-persist on app

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import { AppNavigation } from "./component/navigation/navigation";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <AppNavigation />
-      </View>
+      <PersistGate loading={null} persistor={persistor}>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <AppNavigation />
+        </View>
+      </PersistGate>
     </Provider>
   );
 }
